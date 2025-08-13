733a471df7a1eb46b140a24b982e78c3acb995cd
haeing <hain0817@korea.ac.kr>
2025-08-13 13:08:40 +0900

    bcout param update

--- macro/bcouttracking-residual.py
+++ macro/bcouttracking-residual.py
@@ -20,43 +20,54 @@ ROOT.gStyle.SetOptFit(1)
 #______________________________________________________________________________
 @mh.update_canvas(divisions=(4, 2))
 def residual(c1, name, key, nplane=8, fit=True):
-  if not hasattr(residual, 'ref_off'):
-    residual.ref_off = None
-  logger.info(f'name={name}, key={key}, nplane={nplane}')
-  result_dict = dict()
-  for i in range(nplane):
-    c1.cd(i+1) #.SetLogy()
-    # if key != '':
-    #   ROOT.gPad.SetLogz()
-    hname = f'{name}_Track_Residual{key}_plane{i}{mh.beamflag_for_param}'
-    h1 = mh.get(hname)
-    #if h1:
-    #  h1.Draw('colz')
-    if fit:
-      mean = h1.GetBinCenter(h1.GetMaximumBin())
-      sigma = min(0.4, h1.GetStdDev())
-      params = np.ndarray(3, dtype='float64')
-      params[0] = h1.GetMaximum()
-      params[1] = mean
-      params[2] = sigma
-      limits = [
-        (0,h1.GetMaximum()*10),
-        (mean - 3*sigma, mean + 3*sigma),
-        (0.01, 0.5)
-      ]
+    if not hasattr(residual, 'ref_off'):
+        residual.ref_off = None
+    logger.info(f'name={name}, key={key}, nplane={nplane}')
+    result_dict = dict()
 
-      result = mh.fit_gaus(h1, params=params, limits=limits, autozoom=False)
-      plane_suffix = ['U1', 'UP1', 'V1', 'VP1', 'U2', 'UP2', 'V2', 'VP2'][i]
-        
-      namekey = f'{name}-{plane_suffix}'
-      if namekey == 'BLC2a-U1':
-        residual.ref_off = result.GetParameter(1)
-        result_dict[namekey] = (0.0, result.GetParameter(2)) 
-      else:
-        result_dict[namekey] = (-1*(result.GetParameter(1) - residual.ref_off), result.GetParameter(2))
-    else:
-      h1.Draw('colz')
-  return result_dict
+    sigmas = []        
+    temps  = []        
+
+    plane_suffix_all = ['U1','UP1','V1','VP1','U2','UP2','V2','VP2']
+
+    for i in range(nplane):
+        c1.cd(i+1)
+        hname = f'{name}_Track_Residual{key}_plane{i}{mh.beamflag_for_param}'
+        h1 = mh.get(hname)
+
+        if not fit:
+            h1.Draw('colz')
+            continue
+
+        mean  = h1.GetBinCenter(h1.GetMaximumBin())
+        sigma = h1.GetRMS()
+        params = [h1.GetMaximum(), mean, sigma]
+        limits = [(0, h1.GetMaximum()*10),
+                  (mean-3*sigma, mean+3*sigma),
+                  (0.01, 0.5)]
+
+        result = mh.fit_gaus(h1, params=params, limits=limits, autozoom=False)
+        mu     = result.GetParameter(1)
+        sig    = result.GetParameter(2)
+
+        sigmas.append(sig)
+
+        plane_suffix = plane_suffix_all[i]
+        namekey = f'{name}-{plane_suffix}'
+
+        if namekey == 'BLC2a-U1':
+            residual.ref_off = mu
+            mu_value = 0.0
+        else:
+            mu_value = -1.0 * (mu - residual.ref_off)
+
+        temps.append((namekey, mu_value))
+
+    avg_sigma = float(np.mean(sigmas)) if sigmas else 0.2
+    for namekey, mu_value in temps:
+        result_dict[namekey] = (mu_value, avg_sigma)
+
+    return result_dict
 
 #______________________________________________________________________________
 def single_run(run_info):
