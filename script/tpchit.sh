8665945f9c8d54b7c140c79185850b9aac300e9b
haeing <hain0817@korea.ac.kr>
2026-01-22 16:54:40 +0900

    move to script directory

--- script/tpchit.sh
+++ script/tpchit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_DIR}/.."
+
 # =================================================
 EXAMPLE_USER_CC="example/UserTPCHit.cc"  
 USR_DIR="usr"
