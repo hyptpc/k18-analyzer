a0bd111b89eae7e7a31c514c375cd60fd1083012
r.j.saito <ryuta.saito.q3@dc.tohoku.ac.jp>
2026-02-04 22:21:47 +0900

    Add weighted average logic to HodoCluster

--- src/HodoCluster.cc
+++ src/HodoCluster.cc
@@ -2,17 +2,19 @@
 
 #include "HodoCluster.hh"
 
-#include <cstring>
+#include <algorithm>
 #include <iostream>
-#include <stdexcept>
-#include <string>
-#include <sstream>
+
+#include <TMath.h>
 
 #include "DebugCounter.hh"
 #include "FuncName.hh"
 #include "HodoHit.hh"
-#include "HodoAnalyzer.hh"
 #include "PrintHelper.hh"
+
+#include <std_ostream.hh>
+
+#define USE_WEIGHTED_AVERAGE 0
 
 //_____________________________________________________________________________
 HodoCluster::HodoCluster(const HodoHC& cont,
@@ -57,46 +59,80 @@ HodoCluster::Calculate()
   if(m_hit_container.empty())
     return;
 
+  m_segment   = 0.;
   m_mean_time = 0.;
   m_ctime     = 0.;
-  m_time_diff = 0.;
-  m_de        = 0.;
-  m_segment   = 0.;
-  m_1st_seg   = TMath::QuietNaN();
-  m_1st_time  = (DetectorName() == "BHT") ? DBL_MIN : DBL_MAX;
   m_time0     = 0.;
   m_ctime0    = 0.;
+  m_time_diff = 0.;
+  m_de        = 0.;
+  m_1st_seg   = TMath::QuietNaN();
+  m_1st_time  = (DetectorName() == "BHT") ? DBL_MIN : DBL_MAX;
+  
+  Double_t sum_w = 0.;
 
   for(Int_t i=0; i<m_cluster_size; ++i){
     const auto& hit = m_hit_container[i];
     const auto& index = m_index[i];
-    m_segment   += hit->SegmentId();
-    m_mean_time += hit->MeanTime(index);
-    m_ctime     += hit->CMeanTime(index);
-    m_time0     += hit->Time0(index);
-    m_ctime0    += hit->CTime0(index);
-    m_time_diff += hit->TimeDiff(index);
+    Double_t seg      = hit->SegmentId();
+    Double_t mt       = hit->MeanTime(index);
+    Double_t ctime    = hit->CMeanTime(index);
+    Double_t time0    = hit->Time0(index);
+    Double_t ctime0   = hit->CTime0(index);
+    Double_t time_diff= hit->TimeDiff(index);
+    Double_t de       = hit->DeltaE();
+    
+    // Add weights and accumulate
+#ifdef USE_WEIGHTED_AVERAGE
+    Double_t w = (de > 0.0) ? de : 0.0;
+#else
+    Double_t w = 1.0;
+#endif
+    sum_w += w; // Accumulate total weight (for normalization)
+
+    m_segment   += seg * w;
+    m_mean_time += mt * w;
+    m_ctime     += ctime * w;
+    m_time0     += time0 * w;
+    m_ctime0    += ctime0 * w;
+    m_time_diff += time_diff * w;
+
+    // Find 1st Hit (Time) and calculate dE
     if(hit->GetName() == "BHT"){
-      m_de = TMath::Max(m_de, hit->DeltaE());
-      if(m_1st_time < hit->CMeanTime(index)){
+      m_de = TMath::Max(m_de, de);
+      if(m_1st_time < ctime){ 
         m_1st_seg  = hit->SegmentId();
-        m_1st_time = hit->CMeanTime(index);
+        m_1st_time = ctime;
       }
     }else{
-      m_de += hit->DeltaE();
-      if(hit->CMeanTime(index) < m_1st_time){
+      m_de += de;
+      if(ctime < m_1st_time){
         m_1st_seg  = hit->SegmentId();
-        m_1st_time = hit->CMeanTime(index);
+        m_1st_time = ctime;
       }
     }
   }
 
-  m_segment   /= Double_t(m_cluster_size);
-  m_mean_time /= Double_t(m_cluster_size);
-  m_ctime     /= Double_t(m_cluster_size);
-  m_time0     /= Double_t(m_cluster_size);
-  m_ctime0    /= Double_t(m_cluster_size);
-  m_time_diff /= Double_t(m_cluster_size);
+  // Normalize by total weight
+  if (sum_w <= 0.) {
+#if 0
+    hddaq::cerr << FUNC_NAME << " sum_w is non-positive : " << sum_w << std::endl
+                << "  " << DetectorName() << " Size:" << m_cluster_size << std::endl;
+    for(Int_t i=0; i<m_cluster_size; ++i){
+       hddaq::cerr << "   Seg:" << m_hit_container[i]->SegmentId() 
+                   << " dE:" << m_hit_container[i]->DeltaE() << std::endl;
+    }
+#endif
+    m_is_good = false;
+    return;
+  }
+
+  m_segment   /= sum_w;
+  m_mean_time /= sum_w;
+  m_ctime     /= sum_w;
+  m_time0     /= sum_w;
+  m_ctime0    /= sum_w;
+  m_time_diff /= sum_w;
 
   m_is_good = true;
 }
